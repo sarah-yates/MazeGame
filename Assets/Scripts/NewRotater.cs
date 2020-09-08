220b0e635123ca235630b8566cacf3e4ccfc7e3d
kaitlynkkc <campbellkk123@gmail.com>
2020-09-08 11:32:16 -0500

    Added InputSystem and Rotater Script

---
+++ Assets/Scripts/NewRotater.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewRotater : MonoBehaviour
+{
+    public float speed = 0;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        transform.Rotate(movementY, 0.0f, -(movementX));
+    }
+}
