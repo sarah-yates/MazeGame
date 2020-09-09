1f0965f8d8d0c477420bccda2773d1a6343c530d
sarahfreeze <58789372+sarahfreeze@users.noreply.github.com>
2020-09-08 22:45:45 -0500

    added pickups
    I added pickups but they are broken, they don't disappear when the player hits them.

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
